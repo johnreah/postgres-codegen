9793b04b8881d4e167f5d92a335baba8e2acf49a
johnreah <john.reah@johnreah.com>
2021-10-29 16:11:57 +0100

    Start on Telosys

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,8 @@ docker stop postgres-codegen
 docker image rm johnreah/postgres-codegen
 cp ../accounts-netbeans/src/main/resources/accounts-netbeans.sql ./accounts-netbeans.sql
 cp ../accounts-hibernate/src/main/resources/accounts-hibernate.sql ./accounts-hibernate.sql
+cp ../accounts-telosys-jpa/src/main/resources/accounts-telosys-jpa.sql ./accounts-telosys-jpa.sql
 docker build -t johnreah/postgres-codegen .
 rm accounts-netbeans.sql
 rm accounts-hibernate.sql
+rm accounts-telosys-jpa.sql
