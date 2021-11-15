8710ed57efbc2bb733ad56db70c96c7fd48906ea
johnreah <john.reah@johnreah.com>
2021-11-15 09:33:22 +0000

    Changed configuration so that hibernate.properties (used at code generation time) and application.properties (used at run time) don't interfere with each other and confuse Hikari connection pool.

--- accounts-springdata-jpa/src/main/java/com/johnreah/accounts/springdatajpa/CustomerRepository.java
+++ accounts-springdata-jpa/src/main/java/com/johnreah/accounts/springdatajpa/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.johnreah.accounts.springdatajpa;
 
+import com.johnreah.accounts.springdatajpa.generated.Customer;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
