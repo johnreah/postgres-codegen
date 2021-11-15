62eff5b9d769cc44bf122d6b6ee78dfbceb776a1
johnreah <john.reah@johnreah.com>
2021-11-15 14:05:44 +0000

    More on POJOs and mappers (shallow copy only). Lombok getters & setters.

--- accounts-springdata-jpa/src/main/java/com/johnreah/accounts/springdatajpa/model/mappers/AccountMapper.java
+++ accounts-springdata-jpa/src/main/java/com/johnreah/accounts/springdatajpa/model/mappers/AccountMapper.java
@@ -5,17 +5,34 @@ import com.johnreah.accounts.springdatajpa.model.pojos.Account;
 
 public class AccountMapper {
 
-        public static Account entityToPojo(AccountEntity accountEntity) {
-            Account account = new Account();
-            account.setId(accountEntity.getId());
-            account.setAccountType(AccountTypeMapper.entityToPojo(accountEntity.getAccountTypeEntity()));
-            return account;
-        }
+    public static Account entityToPojo(AccountEntity accountEntity) {
+        Account account = new Account();
 
-        public static AccountEntity pojoToEntity(Account account) {
-            AccountEntity accountEntity = new AccountEntity();
-            accountEntity.setId(account.getId());
-            return accountEntity;
-        }
+        account.setId(accountEntity.getId());
+        account.setAccountType(AccountTypeMapper.entityToPojo(accountEntity.getAccountTypeEntity()));
+        account.setDescription(accountEntity.getDescription());
+        account.setBalance(accountEntity.getBalance());
+        account.setBalanceTimestamp(accountEntity.getBalanceTimestamp());
+        account.setReference(accountEntity.getReference());
+        //CustomerEntities
+        //AccountHistoryEntities
+
+        return account;
+    }
+
+    public static AccountEntity pojoToEntity(Account account) {
+        AccountEntity accountEntity = new AccountEntity();
+
+        accountEntity.setId(account.getId());
+        accountEntity.setAccountTypeEntity(AccountTypeMapper.pojoToEntity(account.getAccountType()));
+        accountEntity.setDescription(account.getDescription());
+        accountEntity.setBalance(account.getBalance());
+        accountEntity.setBalanceTimestamp(account.getBalanceTimestamp());
+        accountEntity.setReference(account.getReference());
+        //CustomerEntities
+        //AccountHistoryEntities
+
+        return accountEntity;
+    }
 
 }
--- accounts-springdata-jpa/src/main/java/com/johnreah/accounts/springdatajpa/model/pojos/Customer.java
+++ accounts-springdata-jpa/src/main/java/com/johnreah/accounts/springdatajpa/model/pojos/Customer.java
@@ -14,46 +14,6 @@ public class Customer {
     private String email;
     private String reference;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
     public String toString() {
         return String.format("%d | %s, %s | %s | %s", this.getId(), this.getLastName(), this.getFirstName(), this.getEmail(), this.getReference());
     }
